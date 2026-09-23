a29d0a87ee8b8c7029fb44f8c1cd09d96685bcd2
akernizan <akernizan@gmail.com>
2026-09-23 14:54:39 -0400

    configure deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/DSA-tutor/",
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.js',
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
     css: true,
   },
 });
